997796ce978669267298ef00f7bbd196920df942
Kostya-Egorov <105317468+Kostya-Egorov@users.noreply.github.com>
2022-05-10 22:44:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,188 @@
+import pygame
+import random
+
+
+class Hero:
+    def __init__(self):
+        self.hero = pygame.image.load("Textures/Hero.png").convert()
+        self.tree = 0
+        self.rock = 0
+        self.count = 0
+        self.name = "Nersop"
+        self.lvl = 0
+
+    def hero_stats(self, font):
+        sc.blit(self.hero, (1300, 15))
+        sc.blit(font.render(self.name, True, WHITE), (1390, 17))
+        sc.blit(font.render("Уровень: " + str(self.lvl), True, WHITE), (1390, 70))
+
+
+def move(a):
+    global stop
+    desk.dsk[hero_index[0]][hero_index[1]] = 0
+    s_x, s_y = hero_index[0], hero_index[1]
+    if a == 1:
+        c_x, c_y = pygame.mouse.get_pos()
+        if 1280 > c_x - (c_x % 80) > 7 * 80 and hero_index[1] < 91:
+            hero_index[1] += 1
+        elif 1280 > c_x - (c_x % 80) < 7 * 80 and hero_index[1] > 0:
+            hero_index[1] -= 1
+        if 1280 > c_x and c_y - (c_y % 80) > 6 * 80 and hero_index[0] < 93:
+            hero_index[0] += 1
+        elif 1280 > c_x and c_y - (c_y % 80) < 6 * 80 and hero_index[0] > 0:
+            hero_index[0] -= 1
+    if a == 2:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and hero_index[1] > 0:
+            hero_index[1] -= 1
+        if keys[pygame.K_RIGHT] and hero_index[1] < 91:
+            hero_index[1] += 1
+        if keys[pygame.K_UP] and hero_index[0] > 0:
+            hero_index[0] -= 1
+        if keys[pygame.K_DOWN] and hero_index[0] < 93:
+            hero_index[0] += 1
+    if desk.count == 0:
+        desk.count += 1
+    else:
+        stop = 0
+        desk.count = 0
+    if 20 <= desk.dsk[hero_index[0]][hero_index[1]] < 24:
+        if stop == 1:
+            desk.tree += 1
+            stop = 0
+            desk.count = 0
+        else:
+            stop += 1
+            hero_index[0], hero_index[1] = s_x, s_y
+    desk.dsk[hero_index[0]][hero_index[1]] = 1
+
+
+class Desk(Hero):
+    def __init__(self, screen):
+        super().__init__()
+        self.dsk = [[0 for _ in range(100)] for _ in range(100)]
+        self.screen = screen
+        self.x, self.y = 0, 0
+
+    def create_desk(self):
+        for i in range(50):
+            for j in range(random.randrange(0, 4), 50, random.randrange(3, 8)):
+                a = random.randrange(0, 4)
+                if a == 0:
+                    self.dsk[i][j] = 20
+                elif a == 1:
+                    self.dsk[i][j] = 21
+                elif a == 2:
+                    self.dsk[i][j] = 22
+                elif a == 3:
+                    self.dsk[i][j] = 23
+        self.dsk[hero_index[0]][hero_index[1]] = 1
+
+    def show_desk(self):
+        x, y = 0, 0
+        if hero_index[1] < 92:
+            for i in range(H // 80):
+                a = ((hero_index[0] - 6) + i) * (((hero_index[0] - 6) + i) > 0)
+                for j in range(16):
+                    if self.dsk[hero_index[0] - 6 + i][hero_index[1] - 7 + j] == 0:
+                        self.screen.blit(grass, (x, y))
+                    elif self.dsk[hero_index[0] - 6 + i][hero_index[1] - 7 + j] == 1:
+                        self.screen.blit(grass, (x, y))
+                        self.hero.set_colorkey((255, 255, 255))
+                        self.screen.blit(self.hero, (x, y))
+                        self.x, self.y = a, j
+                    if self.dsk[hero_index[0] - 6 + i][hero_index[1] - 7 + j] == 20:
+                        tree[0].set_colorkey((255, 255, 255))
+                        self.screen.blit(tree[0], (x, y))
+                    elif self.dsk[hero_index[0] - 6 + i][hero_index[1] - 7 + j] == 21:
+                        tree[1].set_colorkey((255, 255, 255))
+                        self.screen.blit(tree[1], (x, y))
+                    elif self.dsk[hero_index[0] - 6 + i][hero_index[1] - 7 + j] == 22:
+                        tree[2].set_colorkey((255, 255, 255))
+                        self.screen.blit(tree[2], (x, y))
+                    elif self.dsk[hero_index[0] - 6 + i][hero_index[1] - 7 + j] == 23:
+                        tree[3].set_colorkey((255, 255, 255))
+                        self.screen.blit(tree[3], (x, y))
+                    x += 80
+                x = 0
+                y += 80
+    def show_interface(self):
+        sc.blit(interface, (1280, 0))
+        sc.blit(btn_building, (1290, 470))
+        sc.blit(btn_quit, (1290, 950))
+        sc.blit(btn_save, (1290, 770))
+        sc.blit(btn_load, (1290, 860))
+        desk.hero_stats(font)
+        sc.blit(count_tree, [1330, 415])
+        sc.blit(count_rock, [1445, 415])
+
+
+pygame.init()
+
+W, H = 1680, 1040
+
+sc = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Bibus")
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+FPS = 40
+clock = pygame.time.Clock()
+
+pygame.mouse.set_visible(False)
+
+grass = pygame.image.load("Textures/TexturedGrass.png").convert()
+tree1 = pygame.image.load("Textures/Tree1.png").convert()
+tree2 = pygame.image.load("Textures/Tree2.png").convert()
+tree3 = pygame.image.load("Textures/Tree3.png").convert()
+tree4 = pygame.image.load("Textures/Tree4.png").convert()
+tree = [tree1, tree2, tree3, tree4]
+cursor = pygame.image.load("Textures/cursor.png").convert()
+cursor.set_colorkey((255, 255, 255))
+box_selector1 = pygame.image.load("Textures/BoxSelector1.png").convert()
+box_selector2 = pygame.image.load("Textures/BoxSelector2.png").convert()
+box_selector1.set_colorkey((255, 255, 255))
+box_selector2.set_colorkey((255, 255, 255))
+interface = pygame.image.load("Textures/Interface.png").convert()
+btn_building = pygame.image.load("Textures/Buttons/building.png").convert()
+btn_quit = pygame.image.load("Textures/Buttons/quit.png").convert()
+btn_load = pygame.image.load("Textures/Buttons/load.png").convert()
+btn_save = pygame.image.load("Textures/Buttons/save.png").convert()
+all_img = [grass, tree1, tree2, tree3, tree4, cursor, box_selector1]
+
+font = pygame.font.Font(None, 60)
+count_tree = font.render(str(Desk(sc).tree), True, WHITE)
+count_rock = font.render(str(Desk(sc).rock), True, WHITE)
+
+hero_index = [6, 7]
+stop = 0
+
+desk = Desk(sc)
+desk.create_desk()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            move(1)
+            count_tree = font.render(str(desk.tree), True, WHITE)
+        if event.type == pygame.KEYDOWN:
+            move(2)
+            count_tree = font.render(str(desk.tree), True, WHITE)
+    desk.show_desk()
+    c_x, c_y = pygame.mouse.get_pos()
+    if 480 < c_x < 720 and 400 < c_y < 640:
+        if stop == 0:
+            sc.blit(box_selector1, (c_x - (c_x % 80), c_y - (c_y % 80)))
+        else:
+            sc.blit(box_selector2, (c_x - (c_x % 80), c_y - (c_y % 80)))
+    desk.show_interface()
+    sc.blit(cursor, (c_x, c_y))
+    pygame.display.update()
+
+    clock.tick(FPS)
